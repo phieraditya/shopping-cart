e51e3e520f8fa2eeeff2d2f3510ece3aaa0ab41c
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-11-28 17:30:44 +0700

    Load products dynamically

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -4,23 +4,15 @@ import { productsArray } from '../productsStore'
 const Store = () => {
   return (
     <>
-      <h1>Welcome to the Store!</h1>
+      <h1 align="center" className="p-3">
+        Welcome to the Store!
+      </h1>
       <Row xs={1} md={3} className="g-4">
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
+        {productsArray.map((product, idx) => (
+          <Col align="center" key={idx}>
+            <h1>{product.title}</h1>
+          </Col>
+        ))}
       </Row>
     </>
   )
