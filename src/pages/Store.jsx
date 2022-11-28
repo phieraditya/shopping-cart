8f106f8a942a7362c6b97de0407e29eef46a68a7
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-11-28 15:34:28 +0700

    Build rows and cols for Store page

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,5 +1,28 @@
-const Store = () => {
-  return <h1>Welcome to the Store!</h1>;
-};
+import { Row, Col } from 'react-bootstrap'
 
-export default Store;
+const Store = () => {
+  return (
+    <>
+      <h1>Welcome to the Store!</h1>
+      <Row xs={1} md={3} className="g-4">
+        <Col align="center">
+          <h1>Product</h1>
+        </Col>
+        <Col align="center">
+          <h1>Product</h1>
+        </Col>
+        <Col align="center">
+          <h1>Product</h1>
+        </Col>
+        <Col align="center">
+          <h1>Product</h1>
+        </Col>
+        <Col align="center">
+          <h1>Product</h1>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default Store
