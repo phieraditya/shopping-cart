e70d2e6c9a93440e8300b9f5ae486a69bff2f64d
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-11-29 16:24:51 +0700

    Create a modal for the card

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,30 @@
-import { Button, Container, Navbar, Modal } from 'react-bootstrap';
+import { Button, Container, Navbar, Modal } from 'react-bootstrap'
+import { useState } from 'react'
 
 const NavbarComponent = () => {
-  return (
-    <Navbar expand="sm">
-      <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-        <Button>Cart 0 Items</Button>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
 
-export default NavbarComponent;
+  return (
+    <>
+      <Navbar expand="sm">
+        <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Button onClick={handleShow}>Cart 0 Items</Button>
+        </Navbar.Collapse>
+      </Navbar>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Shopping Cart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h1>This is the modal body</h1>
+        </Modal.Body>
+      </Modal>
+    </>
+  )
+}
+
+export default NavbarComponent
