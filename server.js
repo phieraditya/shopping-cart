9fed74c92a059f006a0f1f526bed3b11b023e974
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-12-07 12:17:45 +0700

    Create stripe checkout request

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 app.post('/checkout', async (req, res) => {
   /*
-Data from react
+Data from front-end
 req.body.items
 [
   {
@@ -29,9 +29,34 @@ req.body.items
 Stripe wants
 [
   {
-    price: 99000
+    price: 1
     quantity: 3
   }
 ]
   */
+  console.log(req.body)
+  const items = req.body.items
+  let lineItems = []
+
+  items.forEach((item) => {
+    lineItems.push({
+      price: item.id,
+      quantity: item.quantity,
+    })
+  })
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: 'payment',
+    success_url: 'http://localhost:3000/success',
+    cancel_url: 'http://localhost:3000/cancel',
+  })
+
+  res.send(
+    JSON.stringify({
+      url: session.url,
+    })
+  )
 })
+
+app.listen('4000', () => console.log('Listening on Port 4000!!'))
