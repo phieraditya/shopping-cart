7b2fcb047f4ce7344a7383be40ef38569a0ecca8
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-12-07 13:09:16 +0700

    Add Stripe IDs to React

--- store-fe/src/productsStore.js
+++ store-fe/src/productsStore.js
@@ -1,18 +1,22 @@
+// Panci Susu 20 cm: price_1MAV7WBCzbmNogDuwCAIb0Cm
+// Panci Susu Non Stick 20 cm: price_1MAVBoBCzbmNogDuNOwFK7AX
+// Mesin Cuci Mini: price_1MAVFaBCzbmNogDu032exKAc
+
 const productsArray = [
   {
-    id: '1',
-    title: 'Coffee',
-    price: 24900,
+    id: 'price_1MAV7WBCzbmNogDuwCAIb0Cm',
+    title: 'Panci Susu Stainless Steel',
+    price: 110900,
   },
   {
-    id: '2',
-    title: 'Sunglasses',
-    price: 99900,
+    id: 'price_1MAVBoBCzbmNogDuNOwFK7AX',
+    title: 'Panci Susu Non Stick',
+    price: 85000,
   },
   {
-    id: '3',
-    title: 'Camera',
-    price: 3999000,
+    id: 'price_1MAVFaBCzbmNogDu032exKAc',
+    title: 'Mesin Cuci 4.5 kg',
+    price: 624800,
   },
 ]
 
